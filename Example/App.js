df8dd0057d75442e07dbfaa905fa133c6c01991a
Pranav Chauhan <pranav.chauhan@metricstream.com>
2020-01-28 17:06:44 +0530

    migrating to RN61+

--- Example/App.js
+++ Example/App.js
@@ -4,71 +4,119 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
-import RNShineButton from 'react-native-shine-button'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import RNShineButton from 'react-native-shine-button';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+Icon.loadFont()
 
 export default class App extends Component<{}> {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-      visible: true
-    }
+      visible: true,
+    };
   }
 
-  componentDidMount () {
-    setInterval(
-      () => {
-        this.setState({
-          visible: !this.state.visible
-        })
-      }, 1000
-    )
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        visible: !this.state.visible,
+      });
+    }, 1000);
   }
 
   render() {
-    let music = <Icon family={"FontAwesome"} name={"music"} color={"#808080"} />;
-    let comment = <Icon family={"FontAwesome"} name={"comment"} color={"#808080"} />;
-    let microphone = <Icon family={"FontAwesome"} name={"microphone"} color={"#808080"} />;
-    let play = <Icon family={"FontAwesome"} name={"play"} color={"#808080"} />;
+    let music = (
+      <Icon family={'FontAwesome'} name={'music'} color={'#808080'} />
+    );
+    let comment = (
+      <Icon family={'FontAwesome'} name={'comment'} color={'#808080'} />
+    );
+    let microphone = (
+      <Icon family={'FontAwesome'} name={'microphone'} color={'#808080'} />
+    );
+    let play = <Icon family={'FontAwesome'} name={'play'} color={'#808080'} />;
 
-    return <View style={styles.container}>
+    return (
+      <View style={styles.container}>
         <View style={styles.subContainer}>
-          <RNShineButton shape={"heart"} color={"#808080"} fillColor={"#ff0000"} size={100} value={true} />
-          <RNShineButton shape={"like"} color={"#808080"} fillColor={"#3d7057"} size={100} />
-          <RNShineButton shape={"smile"} color={"#808080"} fillColor={"#703d3d"} size={100} />
-          <RNShineButton shape={"star"} color={"#808080"} fillColor={"#3d3d70"} size={100} />
+          <RNShineButton
+            shape={'heart'}
+            color={'#808080'}
+            fillColor={'#ff0000'}
+            size={100}
+            value={true}
+          />
+          <RNShineButton
+            shape={'like'}
+            color={'#808080'}
+            fillColor={'#3d7057'}
+            size={100}
+          />
+          <RNShineButton
+            shape={'smile'}
+            color={'#808080'}
+            fillColor={'#703d3d'}
+            size={100}
+          />
+          <RNShineButton
+            shape={'star'}
+            color={'#808080'}
+            fillColor={'#3d3d70'}
+            size={100}
+          />
         </View>
         <View style={styles.subContainer}>
-          <RNShineButton shape={music} color={"#808080"} fillColor={"#ff0000"} size={100} value={this.state.visible} />
-          <RNShineButton shape={comment} color={"#808080"} fillColor={"#3d7057"} size={100} value={this.state.visible} />
-          <RNShineButton shape={microphone} color={"#808080"} fillColor={"#703d3d"} size={100} value={this.state.visible} />
-          <RNShineButton shape={play} color={"#808080"} fillColor={"#3d3d70"} size={100} value={this.state.visible} />
+          <RNShineButton
+            shape={music}
+            color={'#808080'}
+            fillColor={'#ff0000'}
+            size={100}
+            value={this.state.visible}
+          />
+          <RNShineButton
+            shape={comment}
+            color={'#808080'}
+            fillColor={'#3d7057'}
+            size={100}
+            value={this.state.visible}
+          />
+          <RNShineButton
+            shape={microphone}
+            color={'#808080'}
+            fillColor={'#703d3d'}
+            size={100}
+            value={this.state.visible}
+          />
+          <RNShineButton
+            shape={play}
+            color={'#808080'}
+            fillColor={'#3d3d70'}
+            size={100}
+            value={this.state.visible}
+          />
         </View>
-      </View>;
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-around",
-    alignItems: "stretch",
-    backgroundColor: "#fafafa"
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    backgroundColor: '#fafafa',
   },
   subContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center"
-  }
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
 });
